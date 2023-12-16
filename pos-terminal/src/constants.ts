2b48e16c18b171954a7c54f38d51de5b943ed28a
Bruno Wu <wubruno@outlook.com>
2023-12-16 14:46:23 +0000

    setting up uniswap

--- pos-terminal/src/constants.ts
+++ pos-terminal/src/constants.ts
@@ -4,9 +4,9 @@ export const chainConfig = {
     vaultFactory: "0xCE2E5022982F3f3E11D199eae5A59de1371748f5",
     posTerminal: "0x473C35c466506c0e235620F348f7F199182489A7",
     rpc: "https://sepolia.mode.network",
-    usdc: "", // TODO
-    usdt: "", // TODO
-    router: "", // TODO
+    usdc: "0xEc61D69964004EAa23Ab76c721824650B9276E0a",
+    usdt: "0xCB87fcb41b60136ED2E414252AEE05b5dC539aB2",
+    router: "0x5951479fE3235b689E392E9BC6E968CE10637A52",
     blockExplorer: "https://sepolia.explorer.mode.network/tx/",
   },
 } as Record<
